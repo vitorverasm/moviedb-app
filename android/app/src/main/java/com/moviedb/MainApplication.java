c41ec80d0a21f05734cc9d01893d6111f8e08f3b
Vitor Veras <vitorverasm@gmail.com>
2019-12-01 20:33:11 -0300

    Link firebase database module

--- android/app/src/main/java/com/moviedb/MainApplication.java
+++ android/app/src/main/java/com/moviedb/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           packages.add(new RNFirebaseAuthPackage());
+          packages.add(new RNFirebaseDatabasePackage());
           return packages;
         }
 
